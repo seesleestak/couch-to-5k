d53b2edc1873ac60c793e8e79ca0843e904cbd4a
seesleestak <calvin.cieslak@gmail.com>
2016-10-31 17:25:09 -0700

    Button component state

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
 class Button extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isStart: true,
+            btnText: 'start'
+        }
+    }
+
+    handleClick() {
+        if(this.state.isStart){
+            this.setState({
+                isStart: !this.state.isStart,
+                btnText: 'stop'
+            })
+        }
+        else {
+            this.setState({
+                isStart: !this.state.isStart,
+                btnText: 'start'
+            })
+        }
+
+    }
 
     render() {
-        return <div className="waves-effect waves-light btn">Click</div>;
+        return <div className="waves-effect waves-light btn" onClick={this.handleClick.bind(this)}>{this.state.btnText}</div>;
     }
 }
 
