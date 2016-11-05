83d592993e43ba73285dcdc5eb35156aa2d768a3
seesleestak <calvin.cieslak@gmail.com>
2016-11-05 15:04:44 -0700

    Finalized plan.js data object

--- client/data/plan.js
+++ client/data/plan.js
@@ -212,4 +212,151 @@ const plan = [
             run: 300
         ]
     },
+    {
+        id: 12,
+        week: 5,
+        day: 1,
+        secSegment: [
+            warmup: 300,
+            run: 300,
+            walk: 180,
+            run: 300,
+            walk: 180,
+            run: 300
+        ]
+    },
+    {
+        id: 13,
+        week: 5,
+        day: 2,
+        secSegment: [
+            warmup: 300,
+            run: 480,
+            walk: 300,
+            run: 480
+        ]
+    },
+    {
+        id: 14,
+        week: 5,
+        day: 3,
+        secSegment: [
+            warmup: 300,
+            run: 1200
+        ]
+    },
+    {
+        id: 15,
+        week: 6,
+        day: 1,
+        secSegment: [
+            warmup: 300,
+            run: 300,
+            walk: 180,
+            run: 480,
+            walk: 180,
+            run: 300
+        ]
+    },
+    {
+        id: 16,
+        week: 6,
+        day: 2,
+        secSegment: [
+            warmup: 300,
+            run: 600,
+            walk: 180,
+            run: 600
+        ]
+    },
+    {
+        id: 17,
+        week: 6,
+        day: 3,
+        secSegment: [
+            warmup: 300,
+            run: 1320
+        ]
+    },
+    {
+        id: 18,
+        week: 7,
+        day: 1,
+        secSegment: [
+            warmup: 300,
+            run: 1500
+        ]
+    },
+    {
+        id: 19,
+        week: 7,
+        day: 2,
+        secSegment: [
+            warmup: 300,
+            run: 1500
+        ]
+    },
+    {
+        id: 20,
+        week: 7,
+        day: 3,
+        secSegment: [
+            warmup: 300,
+            run: 1500
+        ]
+    },
+    {
+        id: 21,
+        week: 8,
+        day: 1,
+        secSegment: [
+            warmup: 300,
+            run: 1680
+        ]
+    },
+    {
+        id: 22,
+        week: 8,
+        day: 2,
+        secSegment: [
+            warmup: 300,
+            run: 1680
+        ]
+    },
+    {
+        id: 23,
+        week: 8,
+        day: 3,
+        secSegment: [
+            warmup: 300,
+            run: 1680
+        ]
+    },
+    {
+        id: 24,
+        week: 9,
+        day: 1,
+        secSegment: [
+            warmup: 300,
+            run: 1800
+        ]
+    },
+    {
+        id: 25,
+        week: 9,
+        day: 2,
+        secSegment: [
+            warmup: 300,
+            run: 1800
+        ]
+    },
+    {
+        id: 26,
+        week: 9,
+        day: 3,
+        secSegment: [
+            warmup: 300,
+            run: 1800
+        ]
+    }
 ]
