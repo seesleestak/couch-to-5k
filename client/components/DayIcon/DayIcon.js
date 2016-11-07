622a034b80849db7292355b00f6e9315fd2593b0
seesleestak <calvin.cieslak@gmail.com>
2016-11-06 22:45:45 -0800

    Timer setup

--- client/components/DayIcon/DayIcon.js
+++ client/components/DayIcon/DayIcon.js
@@ -4,10 +4,18 @@ import css from './DayIcon.css'
 class DayIcon extends Component {
     render() {
         return (
-            <div className="circle">
-                <div className="text-container">
-                    <div>WEEK 1</div>
-                    <div>DAY 1</div>
+            <div className="dayicon-container">
+                <div className="circle-inactive">
+                    <div className="text-container-inactive">
+                        <div>WEEK 1</div>
+                        <div>DAY 1</div>
+                    </div>
+                </div>
+                <div className="circle-active">
+                    <div className="text-container-active">
+                        <div>WEEK 1</div>
+                        <div>DAY 1</div>
+                    </div>
                 </div>
             </div>
         )
--- client/components/TimerMain/TimerMain.js
+++ client/components/TimerMain/TimerMain.js
@@ -2,10 +2,38 @@ import React, { Component } from 'react'
 import css from './TimerMain.css'
 
 class TimerMain extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsRemaining: 10
+        }
+    }
+    getInitialState() {
+        return {
+            secondsRemaining: 10
+        };
+    }
+
+    tick() {
+        this.setState({secondsRemaining: this.state.secondsRemaining - 1})
+        if (this.state.secondsRemaining <= 0) {
+            clearInterval(this.interval)
+        }
+    }
+
+    componentDidMount() {
+        this.setState({secondsRemaining: this.state.secondsRemaining})
+        this.interval = setInterval(this.tick, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
     render() {
         return (
             <div>
-                <h2>Timer</h2>
+                <h2>{this.state.secondsRemaining}</h2>
                 <h4>Time elapsed</h4>
             </div>
         )
